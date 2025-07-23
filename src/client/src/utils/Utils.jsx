18b8a4bf0ed1e6f22943b6bf2421fbcd7270a351
Erik Brandon <erikbrandon1@gmail.com>
2025-07-23 18:27:26 -0400

    Containerized & Added jService API to Project - In `docker-compose.yaml`, added postgres and jservice services along   with additional properties including healthchecks, env file paths and   depends_on attributes. - In `src/client/.env.example`, added some initial example environment   variable values for the client application. - In `src/client/src/apis/JService.jsx`, replaced hardcoded API request   URLs with concatenated template strings to make it easy to customize   the client application environment. Also made useQueryAnswers more   reliable by querying the total number of categories before requesting   a random set of categories. - In `src/client/src/apis/MongoDB.jsx`, replaced hardcoded API request   URLs with concatenated template strings to make it easy to customize   the server application environment. - In `src/client/src/utils/Utils.jsx`, added a splitRangeIntoIntervals   function that can return an array of intervals to be used in   JService.jsx to request six random categories each within a separate   range of the total number of categories available in the jService API. - In `src/jservice/.env.example`, added some initial example environment   variable values for the jService API. - In `src/jservice/Dockerfile`, created a two stage Dockerfile that   pulls down a copy of the jService API from a third-party GitHub repo   and then copies in local overrides to fix a few bugs and properly   configure the resulting Docker container. - In `src/jservice/entrypoint.sh`, created an entrypoint script that is   used in the jservice Dockerfile to create the postgres database tables   and run migrations along with fetching clues from the two latest   seasons of Jeopardy from the jArchive website. - In `src/jservice/overrides/app/models/category.rb`, added a modified   category model that fixes a frozen hash error by duplicating the   options argument first before any other steps, to prevent accidental   mutation in case it was frozen prior to the function call. - In `src/jservice/overrides/config/database.yml`, added a modified   database config file that overrides the database connection settings   with environment variables passed in from the jservice .env file.

--- src/client/src/utils/Utils.jsx
+++ src/client/src/utils/Utils.jsx
@@ -1,16 +1,38 @@
 const Utils = {};
 
 Utils.generateRandomNumber = (min, max) =>
-    Math.floor(Math.random() * (max - min + 1)) + min;
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+Utils.splitRangeIntoIntervals = (min, max, numIntervals) => {
+  if (numIntervals <= 0) {
+    return [];
+  }
+
+  const intervalSize = (max - min) / numIntervals;
+  const intervals = [];
+
+  for (let i = 0; i < numIntervals; i++) {
+    const start = min + i * intervalSize;
+    let end = start + intervalSize;
+
+    if (i === numIntervals - 1) {
+      end = max;
+    }
+
+    intervals.push([Math.ceil(start), Math.floor(end)]);
+  }
+
+  return intervals;
+};
 
 Utils.waitForSeconds = (seconds) =>
-    new Promise((resolve) => setTimeout(resolve, seconds));
+  new Promise((resolve) => setTimeout(resolve, seconds));
 
 Utils.calculateSuccessRate = (correctAnswers, attemptedAnswers) => {
-    let successRate = Math.round(
-        (parseInt(correctAnswers) / parseInt(attemptedAnswers)) * 100
-    );
-    return isNaN(successRate) ? '0%' : `${successRate.toString()}%`;
+  let successRate = Math.round(
+    (parseInt(correctAnswers) / parseInt(attemptedAnswers)) * 100,
+  );
+  return isNaN(successRate) ? '0%' : `${successRate.toString()}%`;
 };
 
 export default Utils;
